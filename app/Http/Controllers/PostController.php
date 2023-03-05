b50e51e7955b8fe35a923e4907e354fc904f68cc
jonno2012 <jonathanrhill78@gmail.com>
2023-03-05 15:31:14 +0000

    Select only specific fields of eager loaded rels

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $years = Post::query()
+            ->select('id', 'title', 'slug', 'published_at', 'author_id')
+            ->with('author:id,name')
+            ->latest('published_at')
+            ->get()
+            ->groupBy(fn ($post) => $post->published_at->year);
+
+        return view('posts', ['years' => $years]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}

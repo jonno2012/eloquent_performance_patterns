1b0e2976611cc2e4725a9ee400e8e3e6b980c015
jonno2012 <jonathanrhill78@gmail.com>
2023-03-08 08:37:45 +0000

    Using Unions to run queries independently

--- app/Models/User.php
+++ app/Models/User.php
@@ -105,13 +105,22 @@ class User extends Authenticatable
         // the user will have to use "" around a term to be used as a complete term.
         collect(str_getcsv($terms, ' ', '"'))->filter()->each(function($term) use ($query) {
             $term = $term.'%';
-            $query->where(function ($query) use ($term) {
-                $query->where('first_name', 'like', $term)
-                    ->orWhere('last_name', 'like', $term)
-                    ->orWhereIn('company_id', function($query) use ($term) {
-                        $query->where('name', 'like', $term)
-                            ->pluck('id');
-                    });
+
+            $query->whereIn('id', function($query) use ($term) {
+                $query->select('id')
+                    ->from(function ($query) use ($term) {
+                        $query->select('id')
+                            ->from('users')
+                            ->where('first_name', 'like', $term)
+                            ->orWhere('last_name', 'like', $term)
+                            ->union(
+                                $query->newQuery()
+                                ->select('users.id')
+                                ->from('users')
+                                ->join('companies', 'companies.id', '=', 'users.company_id')
+                                ->where('companies.name', 'like', $term)
+                            );
+                    }, 'matches');
             });
         }); // this now runs as 3 separate queries but each query runs ultra-fast because the query now uses the
         // indexes.
